732e8893b0016f5a6c799059165909dd04d78a68
Pradnya Santosh Salve <71832549+prad451@users.noreply.github.com>
2022-06-06 20:20:27 +0530

    Add files via upload

---
+++ Introduction/C++ introduction.cpp
@@ -0,0 +1,26 @@
+//C++ introduction
+/* What is C++
+-->high level object oriented programming language.
+-->developed by Bjarne Stroustrup
+-->is an extension of C language
+
+Why C++
+-->one of the world's most popular programming languages.
+-->is portable and can be used to develop applications that can be adapted to multiple platforms.
+-->can be found in today's operating systems, Graphical User Interfaces, and embedded systems.
+-->is an object-oriented programming language which gives a clear structure to programs and allows code to be reused, lowering development costs.
+
+Difference between C and C++
+-->C++ was developed as an extension of C, and both languages have almost the same syntax.
+-->The main difference between C and C++ is that C++ support classes and objects, while C does not.
+
+
+
+
+
+
+
+
+
+
+
