c3e7d2029c8a06ad52900f6ada650a96d7d87248
Pradnya Santosh Salve <71832549+prad451@users.noreply.github.com>
2022-06-11 19:36:23 +0530

    Add files via upload

--- Functions/C++ functions.cpp
+++ Functions/C++ functions.cpp
@@ -292,3 +292,32 @@ int main()
 	return 0;
 }
 
+/*C++ Function Overriding
+>>If derived class defines same function as defined in its base class,it is known as function overriding in C++. 
+>>It is used to achieve runtime polymorphism/late binding/dynamic polymorphism. 
+>>It enables us to provide specific implementation of the function which is already provided by its base class.*/
+
+#include <iostream>  
+using namespace std;  
+class Animal 
+{  
+    public:  
+    void eat()
+    {    
+       cout<<"Eating...";    
+    }      
+};   
+class Dog: public Animal    
+{    
+    public:  
+    void eat()    
+    {    
+       cout<<"Eating bread...";    
+    }    
+};  
+int main(void) 
+{  
+   Dog d = Dog();    
+   d.eat();  
+   return 0;  
+}  
